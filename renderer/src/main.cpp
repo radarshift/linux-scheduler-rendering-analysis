4fa4c7f42c11432d7091063cbe4c4d60750176bd
Ari <arielbdanon@gmail.com>
2026-05-06 12:52:38 -0500

    opengl boilerplate

--- renderer/src/main.cpp
+++ renderer/src/main.cpp
@@ -3,34 +3,32 @@
 #include <iostream>
 
 int main() {
-    if (!glfwInit()) return -1;
-
-    // Set OpenGL version to 3.3 Core
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", NULL, NULL);
-    if (!window) {
+    GLFWwindow* window;
+    window = glfwCreateWindow(800,600,"LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
-    // Initialize GLAD before calling any OpenGL functions
+    
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    while (!glfwWindowShouldClose(window)) {
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
+    glViewport(0, 0, 800, 600);
+    while(!glfwWindowShouldClose(window)) {
         glfwSwapBuffers(window);
-        glfwPollEvents();
+        glfwPollEvents();    
     }
-
-    glfwTerminate();
+    
     return 0;
 }
